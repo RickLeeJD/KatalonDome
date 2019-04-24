444fdd37da99e5581221d0c3cd7148c46b536acd
Rick <rickleejd@gmail.com>
2019-04-24 11:21:39 +0800

    Add katalon Dome V1.0

---
+++ Test Suites/Test Suite.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>a21514ec-1cbe-411b-bba9-b018da2fb657</testSuiteGuid>
+   <testCaseLink>
+      <guid>c3e09747-a956-4362-a276-0823431c767a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Demo</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
